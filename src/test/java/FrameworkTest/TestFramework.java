cecb46ae39fdd8568c633fe4fb96c4de0111f2b0
Apurva4222 <apurvanaik2023@gmail.com>
2023-04-27 16:10:45 +0530

    Framework Changes from A

---
+++ src/test/java/FrameworkTest/TestFramework.java
@@ -0,0 +1,11 @@
+package FrameworkTest;
+
+import org.testng.annotations.Test;
+
+public class TestFramework {
+
+    @Test
+    public void testNg(){
+        System.out.println("TestNG Framework");
+    }
+}
